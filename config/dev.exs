834af16666391bfb69228ed3db942bbb87d278de
Assisneto Damasceno <netodamasceno12@gmail.com>
2022-06-15 16:10:03 -0300

    add docker

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :pokex, Pokex.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "pokex_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
