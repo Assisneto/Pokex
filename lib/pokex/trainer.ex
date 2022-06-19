61150cfd6e165c20b1e28097a866000c71d6be7a
Assisneto Damasceno <netodamasceno12@gmail.com>
2022-06-19 17:59:16 -0300

    add migration and changeset of trainer

---
+++ lib/pokex/trainer.ex
@@ -0,0 +1,38 @@
+defmodule Pokex.Trainer do
+  @moduledoc """
+  Changeset of Trainers
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+
+  schema "trainers" do
+    field :name, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
+    timestamps()
+  end
+
+  @required_params [:name, :password]
+
+  def build(params) do
+    params
+    |> changeset()
+    |> apply_action(:insert)
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password, min: 6)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset),
+    do: change(changeset, Argon2.add_hash(password))
+
+  defp put_pass_hash(changeset), do: changeset
+end
