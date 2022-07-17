2fd984a6d0fdee1c56244d9e66373450bbfbae77
Assisneto Damasceno <netodamasceno12@gmail.com>
2022-07-17 14:56:06 -0300

    client http

---
+++ lib/pokex/poke_api/client.ex
@@ -0,0 +1,19 @@
+defmodule Pokex.PokeApi.Client do
+  @moduledoc """
+  Module of Client http
+  """
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "https://pokeapi.co/api/v2"
+  plug Tesla.Middleware.JSON
+
+  def get_pokemon(name),
+    do:
+      "/pokemon/#{name}"
+      |> get()
+      |> handle_get()
+
+  defp handle_get({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, body}
+  defp handle_get({:ok, %Tesla.Env{status: 404}}), do: {:error, "Pokemon not found!"}
+  defp handle_get({:error, _reason} = error), do: error
+end
--- mix.exs
+++ mix.exs
@@ -45,7 +45,9 @@ defmodule Pokex.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
