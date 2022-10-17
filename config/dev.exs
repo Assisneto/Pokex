1d01d4dc11203520a30f2cc91e7c90a4f3044931
Assisneto Damasceno <netodamasceno12@gmail.com>
2022-10-17 18:11:57 -0300

    change docker config

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :pokex, Pokex.Repo,
   username: System.get_env("POSTGRES_USER", "postgres"),
   password: System.get_env("POSTGRES_PASSWORD", "postgres"),
-  hostname: System.get_env("POSTGRES_HOST", "db"),
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
   database: "pokex_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
